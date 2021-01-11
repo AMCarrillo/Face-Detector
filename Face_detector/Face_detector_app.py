c0b1a75de23644a986175a51b130ce8eb2c4bfa2
AMCarrillo <72003315+AMCarrillo@users.noreply.github.com>
2021-01-11 15:14:12 -0600

    Update Face_detector_app.py

--- Face_detector/Face_detector_app.py
+++ Face_detector/Face_detector_app.py
@@ -18,14 +18,14 @@ while True:
     #Detect objects of different sizes in the input image
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-    #Draw rectangles aorund the faces. The last numbres is the thicknes of the line
+    #Draw rectangles around the faces. The last number is the thicknes of the line
     for (x, y, w, h) in face_coordinates:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
   
     cv2.imshow('Face_detector_app', frame)
     key = cv2.waitKey(1)
 
-    #Stop is Q is pressed
+    #To stop it with Q or q
     if key==81 or key==113:
         break
 
